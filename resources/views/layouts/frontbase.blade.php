fa4b521fc017e915c669bffa10933d429a126cc4
alpercankarakas1 <alper.can.karakas@hotmail.com>
2022-04-20 00:41:15 +0300

    web template implementation

--- resources/views/layouts/frontbase.blade.php
+++ resources/views/layouts/frontbase.blade.php
@@ -1,22 +1,60 @@
 <html>
 
 <head>
-    <title>App Name - @yield('title')</title>
-    @yield('head')
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>@yield("title")</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- <link rel="manifest" href="site.webmanifest"> -->
+    <link rel="shortcut icon" type="image/x-icon" href="img/favicon.png">
+    <!-- Place favicon.ico in the root directory -->
+
+    <!-- CSS here -->
+    <link rel="stylesheet" href="{{asset('assets')}}/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/magnific-popup.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/themify-icons.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/nice-select.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/flaticon.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/gijgo.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/animate.min.css">
+    <link rel="stylesheet" href="{{asset('assets')}}/css/slicknav.css">
+
+    <link rel="stylesheet" href="{{asset('assets')}}/css/style.css">
+    <!-- <link rel="stylesheet" href="css/responsive.css"> -->
+    @yield("head")
 </head>
 
 <body>
-<h1>Header</h1>
-@section('sidebar')
-    This is the master sidebar.
-@show
+<!-- header -->
+@include("home.header")
+<!-- /header -->
 
-<div class="container">
-    @yield('content')
-</div>
-<h1>Footer</h1>
+<!-- slider -->
+@section("slider")
+@show
+<!-- /slider -->
+
+
+<!-- sidebar -->
+@section('sidebar')
+    @include("home.sidebar")
+@show
+<!-- /sidebar -->
+
+
+<!-- index -->
+@yield('content')
+<!-- /index -->
+
+
+<!-- footer -->
+@include("home.footer")
 @yield('foot')
+<!-- /footer -->
 
 </body>
-
 </html>
