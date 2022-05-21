77a6debe6f70054d460c388d63eb1eb03f377430
alpercankarakas1 <alper.can.karakas@hotmail.com>
2022-05-21 19:55:41 +0300

    admin panel category list page (getting and listing data from database)

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -11,9 +11,42 @@
         <section class="wrapper site-min-height">
             <h3><i class="fa fa-angle-right"></i> Category List</h3>
             <div class="row mt">
-                <div class="col-lg-12">
-                    <p>Place your content here.</p>
-                </div>
+                <div class="col-md-12">
+                    <div class="content-panel">
+                        <h4><i class="fa fa-angle-right"></i> Category List</h4><hr><table class="table table-bordered table-responsive table-striped">
+
+
+                            <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>Keywords</th>
+                                <th>Description</th>
+                                <th>Image</th>
+                                <th>Status</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                                <th>Show</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach($data as $rs)
+                            <tr>
+                                <td>{{$rs->id}}</td>
+                                <td>{{$rs->title}}</td>
+                                <td>{{$rs->keywords}}</td>
+                                <td>{{$rs->description}}</td>
+                                <td>{{$rs->image}}</td>
+                                <td>{{$rs->status}}</td>
+                                <td><a href="/admin/category/edit{{$rs->id}}"class="btn btn-primary">Edit</a></td>
+                                <td><a href="/admin/category/delete{{$rs->id}}" class="btn btn-danger">Delete</a></td>
+                                <td ><a href="/admin/category/show{{$rs->id}}"class="btn btn-success">Show</a></td>
+                            </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div><!-- /content-panel -->
+                </div><!-- /col-md-12 -->
             </div>
 
         </section><!--/wrapper -->
