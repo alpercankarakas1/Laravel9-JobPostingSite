e6226b04c0683af3d40a1f6ee4a23463db8e5407
alpercankarakas1 <alper.can.karakas@hotmail.com>
2022-05-21 20:34:17 +0300

    admin panel category edit page (updating data)

--- resources/views/admin/category/edit.blade.php
+++ resources/views/admin/category/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.adminbase')
 
-@section('title', 'Add Category')
+@section('title', 'Edit Category')
 
 @section('content')
     <!-- **********************************************************************************************************************************************************
@@ -9,31 +9,31 @@
     <!--main content start-->
     <section id="main-content">
         <section class="wrapper site-min-height">
-            <h3><i class="fa fa-angle-right"></i> Add Category</h3>
+            <h3><i class="fa fa-angle-right"></i> Edit Category</h3>
             <div class="row mt">
                 <div class="col-lg-12">
                     <div class="form-panel">
                         <h4 class="mb"><i class="fa fa-angle-right"></i> Category Elements</h4>
-                        <form class="form-horizontal style-form" action="/admin/category/store" method="post">
+                        <form class="form-horizontal style-form" action="/admin/category/update/{{$data->id}}" method="post">
                             @csrf
                             <div class="form-group">
                                 <label class="col-sm-2 col-sm-2 control-label">Title</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" name="title">
+                                    <input type="text" class="form-control" name="title" value="{{$data->title}}">
                                 </div>
                             </div>
 
                             <div class="form-group">
                                 <label class="col-sm-2 col-sm-2 control-label">Keywords</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" name="keywords">
+                                    <input type="text" class="form-control" name="keywords" value="{{$data->keywords}}">
                                 </div>
                             </div>
 
                             <div class="form-group">
                                 <label class="col-sm-2 col-sm-2 control-label">Description</label>
                                 <div class="col-sm-10">
-                                    <input type="text" class="form-control" name="description">
+                                    <input type="text" class="form-control" name="description" value="{{$data->description}}">
                                 </div>
                             </div>
 
@@ -48,7 +48,7 @@
                                 <label class="col-sm-2 col-sm-2 control-label">Status</label>
                                 <div class="col-sm-10">
                                 <select class="form-control" name="status">
-                                    <option disabled selected value>-- select an option --</option>
+                                    <option disabled selected value>{{$data->status}}</option>
                                     <option>True</option>
                                     <option>False</option>
                                 </select>
@@ -56,7 +56,7 @@
                             </div>
 
 
-                            <button type="submit" class="btn btn-primary btn-lg btn-block">Save</button>
+                            <button type="submit" class="btn btn-primary btn-lg btn-block">Update Data</button>
 
 
                         </form>
